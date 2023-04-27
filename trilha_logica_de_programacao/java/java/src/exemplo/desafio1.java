29e878d8c48766ac0082b10da378079e3249c483
Danillo <danillo.boing.souza@gmail.com>
2023-04-26 21:36:24 -0300

    New: Trilha de Java

---
+++ trilha_logica_de_programacao/java/java/src/exemplo/desafio1.java
@@ -0,0 +1,19 @@
+package exemplo;
+import java.util.Scanner;
+
+public class desafio1 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Insira seu nome: ");
+    String nome = scanner.nextLine();
+    System.out.println("Insira sua idade: ");
+    int idade = scanner.nextInt();
+
+    System.out.println("\nOlá, " + nome + "! Vi que você tem " + idade + " ano(s).\n");
+
+    scanner.close();
+
+  }
+}
